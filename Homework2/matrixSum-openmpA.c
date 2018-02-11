f88d0a7cb9c5386bcd7ae2dfad758fba82a918cc
Emma Good <emma.good@hotmail.se>
2018-02-11 19:47:47 +0100

    Finish HW2

---
+++ Homework2/matrixSum-openmpA.c
@@ -0,0 +1,111 @@
+/* matrix summation using OpenMP
+
+   usage with gcc (version 4.2 or higher required):
+     gcc -O -fopenmp -o matrixSum-openmp matrixSum-openmp.c
+     ./matrixSum-openmp size numWorkers
+
+*/
+
+#include <omp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+#define MAXSIZE 10000  /* maximum matrix size */
+#define MAXWORKERS 8   /* maximum number of workers */
+#define MAXVALUE 100
+
+double start_time, end_time;
+
+int numWorkers;
+int size;
+int matrix[MAXSIZE][MAXSIZE];
+void *Worker(void *);
+
+/* read command line, initialize, and create threads */
+int main(int argc, char *argv[]) {
+  int i, j, total = 0;
+  bool print = false;
+
+  /* read command line args if any */
+  size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
+  numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
+  print = (argc > 3)? true : false;
+  if (size > MAXSIZE) size = MAXSIZE;
+  if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
+
+  omp_set_num_threads(numWorkers);
+
+  //int matrix[MAXSIZE][MAXSIZE];
+
+  /* initialize the matrix */
+  srand(time(NULL));
+  for (i = 0; i < size; i++) {
+      if(print) printf("[ ");
+	  for (j = 0; j < size; j++) {
+      matrix[i][j] = rand()% MAXVALUE;
+      	  if(print) printf(" %d", matrix[i][j]);
+	  }
+	  	  if(print) printf(" ]\n");
+  }
+
+  int max = 0;
+  int min = MAXVALUE;
+  int maxindex[2];
+  int minindex[2];
+  int maxindexes[numWorkers * 2];
+  int minindexes[numWorkers * 2];
+  for(i = 0; i < numWorkers * 2; i++){
+      maxindexes[i] = 0;
+      minindexes[i] = 0;
+  }
+  bool used[numWorkers];
+  for(i = 0; i < numWorkers; i++)
+    used[i] = false;
+
+  start_time = omp_get_wtime();
+#pragma omp parallel for reduction (+:total) reduction (max:max) reduction (min:min) private(j)
+  for (i = 0; i < size; i++){
+    int id = omp_get_thread_num();
+    used[id] = true;
+    //printf("Thread ID: %d on row %d\n", omp_get_thread_num(), i);
+    for (j = 0; j < size; j++){
+      total += matrix[i][j];
+      if(matrix[i][j] > max){
+        max = matrix[i][j];
+        maxindexes[id * 2] = i;
+        maxindexes[(id * 2) + 1] = j;
+      }
+      if(matrix[i][j] < min){
+        min = matrix[i][j];
+        minindexes[id * 2] = i;
+        minindexes[(id * 2) + 1] = j;
+       }
+    }
+  }
+
+    for(i = 0; i < numWorkers * 2; i += 2){
+        //printf("i: %d\n", i);
+        //printf("maxx i: %d maxx i + 1: %d\n", maxindexes[i], maxindexes[i + 1]);
+        if(!used[i/2]) continue;
+        if(matrix[maxindexes[i]][maxindexes[i + 1]] == max){
+            maxindex[0] = maxindexes[i];
+            maxindex[1] = maxindexes[i + 1];
+        }
+        if(matrix[minindexes[i]][minindexes[i + 1]] == min){
+            minindex[0] = minindexes[i];
+            minindex[1] = minindexes[i + 1];
+        }
+    }
+
+// implicit barrier
+
+  end_time = omp_get_wtime();
+
+  printf("the total is %d\n", total);
+  printf("The maximum is %d at (%d, %d)\n", max, maxindex[0] + 1, maxindex[1] + 1);
+  printf("the minimum is %d at (%d, %d)\n", min, minindex[0] + 1, minindex[1] + 1);
+  printf("it took %g seconds\n", end_time - start_time);
+
+}
---
+++ Homework2/quicksort.c
@@ -0,0 +1,130 @@
+#ifndef _REENTRANT
+#define _REENTRANT
+#endif
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+#define MAXSIZE 20
+#define MAXWORKERS 4
+
+void* quicksort(void* struc);
+int partitioning(int low, int high);
+
+double start_time, end_time; /* start and end times */
+
+/* timer - copied from matrixSum.c */
+double read_timer() {
+    static bool initialized = false;
+    static struct timeval start;
+    struct timeval end;
+    if( !initialized ){
+        gettimeofday( &start, NULL );
+        initialized = true;
+    }
+    gettimeofday( &end, NULL );
+    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+int i;
+int numWorkers;
+int arraySize;
+int *a;
+
+struct Part{
+    int low;
+    int high;
+};
+
+int main(int argc, char *argv[]){
+    bool print = false;
+    arraySize = (argc > 1)? atoi(argv[1]) : 15;
+    numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
+    print = (argc > 3)? true : false;
+
+    if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
+
+    //omp_set_num_threads(numWorkers);
+
+    struct Part partition = {0, arraySize - 1};
+
+    a = malloc(sizeof(int) * arraySize);
+
+    srand(time(NULL));
+    for(i = 0; i < arraySize; i++){
+        a[i] = rand() % 1000;
+        //printf("%d ", a[i]);
+    }
+
+    printf("\n");
+    start_time = read_timer();
+    pthread_t start;
+    //pthread_create(&start, NULL, quicksort, &partition);
+    //pthread_join(start, NULL);
+    quicksort(&partition);
+    end_time = read_timer();
+
+    //printf("sorted??: ");
+    if(print){
+        for(i = 0; i < arraySize; i++){
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+    }
+
+    printf("sorted in: %g\n", end_time - start_time);
+    pthread_exit(NULL);
+}
+
+void swap(int a[], int i, int j){
+    int temp;
+    temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+
+void* quicksort(void* struc){
+    struct Part * range = (struct Part *) struc;
+    int low = range->low;
+    int high = range->high;
+    pthread_t th1, th2;
+
+    if(low < high){
+        if(low < 0)
+            low = 0;
+
+        int p = partitioning(low, high);
+
+        struct Part left = {low, p - 1};
+        struct Part right = {p + 1, high};
+        #pragma omp task
+            //struct Part left = {low, p - 1};
+            quicksort(&left);
+
+        #pragma omp task
+            //struct Part right = {p + 1, high};
+            quicksort(&right);
+
+        #pragma omp taskwait
+        }
+}
+
+int partitioning(int low, int high){
+
+    int pivot = a[high];
+    int wall = low - 1;
+    int current;
+
+    for(current = low; current <= high - 1; current++){
+        if(a[current] < pivot){
+            wall += 1;
+            swap(a, wall, current);
+        }
+    }
+    if(a[high] < a[wall + 1]){
+        swap(a, wall + 1, high);
+    }
+    return wall + 1;
+}
